c655133af82b044881c90098dd804ed399b3cec3
Luis Fernando Casas Borja <luisfernandocasasborja@gmail.com>
2017-11-12 22:26:34 -0600

    Creation of Change transform function

--- Assets/Scripts/SetPosition.cs
+++ Assets/Scripts/SetPosition.cs
@@ -8,4 +8,14 @@ public class SetPosition : MonoBehaviour
     public Vector3[] houseRotations;
     public GameObject hauntedHouse;
     private int actualPosition = 0;
+
+    /// <summary>
+    /// Se encarga de modificar el transform de la casa embrujada en base al contenido de housePositions y houseRotations
+    /// </summary>
+    /// <param name="increment"></param>
+    public void ChangeTransform(int increment)
+    {
+        //Se aumenta el valor de actualPosition en base al incremento
+        actualPosition += increment;
+    } //Fin de ChangeTransform()
 } //Fin de ChangeTransform
