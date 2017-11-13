64c1e80ac8adfdad256354bfcbdeba3ef381a2af
Luis Fernando Casas Borja <luisfernandocasasborja@gmail.com>
2017-11-12 22:23:15 -0600

    Creation of Position changing script. Variables declaration

---
+++ Assets/Scripts/SetPosition.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetPosition : MonoBehaviour
+{
+    public Vector3[] housePositions;
+    public Vector3[] houseRotations;
+    public GameObject hauntedHouse;
+    private int actualPosition = 0;
+} //Fin de ChangeTransform
